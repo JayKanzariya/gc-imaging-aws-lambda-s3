a3ca921dab29abb248ddbe1990f74db603b3e352
Nilay Vishwakarma <30928429+iwannabebot@users.noreply.github.com>
2018-12-26 14:15:05 +0530

    Update GcImagingOperations.cs

--- GCImagingAWSLambdaS3/GcImagingOperations.cs
+++ GCImagingAWSLambdaS3/GcImagingOperations.cs
@@ -9,47 +9,15 @@ namespace GCImagingAWSLambdaS3
 {
     public class GcImagingOperations
     {
-        public static string GetGrayScale(Stream stream)
+        public static string GetConvertedImage(Stream stream)
         {
             using (var bmp = new GcBitmap())
             {
                 bmp.Load(stream);
-                bmp.ApplyEffect(GrayscaleEffect.Get(GrayscaleStandard.BT601));
-                return GetBase64(bmp);
-            }
-        }
-
-        public static string GetThumbnail(Stream stream)
-        {
-            using (var origBmp = new GcBitmap())
-            {
-                origBmp.Load(stream);
-                var bmp = origBmp.Resize(100, 100, InterpolationMode.NearestNeighbor);
-                return GetBase64(bmp);
-            }
-        }
-
-        public static string GetEnlarged(Stream stream)
-        {
-            using (var origBmp = new GcBitmap())
-            {
-                origBmp.Load(stream);
-                var bmp = origBmp.Resize(
-                    origBmp.PixelWidth * 2, 
-                    origBmp.PixelHeight * 2,
-                    InterpolationMode.NearestNeighbor);
-                return GetBase64(bmp);
-            }
-        }
-
-        public static string GetWaterMarked(Stream stream)
-        {
-            using (var bmp = new GcBitmap())
-            {
-                bmp.Load(stream);
+                // Add watermark
                 using (var g = bmp.CreateGraphics(Color.White))
                 {
-                    g.DrawString("Watermark", new TextFormat
+                    g.DrawString("This is a watermarked string", new TextFormat
                     {
                         FontSize = 96,
                         ForeColor = Color.FromArgb(128, Color.Yellow),
@@ -58,54 +26,14 @@ namespace GCImagingAWSLambdaS3
                     new RectangleF(0, 0, bmp.Width, bmp.Height),
                     TextAlignment.Center, ParagraphAlignment.Center, false);
                 }
-                return GetBase64(bmp);
+                // Convert to grayscale
+                bmp.ApplyEffect(GrayscaleEffect.Get(GrayscaleStandard.BT601));
+                // Resize to thumbnail
+                var resizedImage = bmp.Resize(100, 100, InterpolationMode.NearestNeighbor);
+                return GetBase64(resizedImage);
             }
         }
-
-        public static string GetCustomImage(Stream stream)
-        {
-            using (var bmp = new GcBitmap())
-            {
-                bmp.Load(stream);
-                // Perform custom operations
-                using (var g = bmp.CreateGraphics(Color.White))
-                {
-                    // draw a solid red border on the image
-                    g.DrawRectangle(
-                        new RectangleF(0, 0, bmp.Width, bmp.Height),
-                        Color.Red,
-                        2,
-                        DashStyle.Solid);
-                    // Add a yellow watermark at bottom right corner
-                    var wmRect = g.MeasureString(
-                        "Created using GCImagingAWSLambdaS3 Lambda", 
-                        new TextFormat
-                        {
-                            FontSize = 18,
-                            ForeColor = Color.FromArgb(128, Color.Yellow),
-                            Font = FontCollection.SystemFonts.DefaultFont
-                        });
-                    g.DrawString(
-                        "Created using GCImagingAWSLambdaS3 Lambda",
-                        new TextFormat
-                        {
-                            FontSize = 18,
-                            ForeColor = Color.FromArgb(128, Color.Yellow),
-                            Font = FontCollection.SystemFonts.DefaultFont
-                        },
-                        new RectangleF(
-                            bmp.Width - wmRect.Width,
-                            bmp.Height - wmRect.Height,
-                            wmRect.Width,
-                            wmRect.Height),
-                        TextAlignment.Center, 
-                        ParagraphAlignment.Center,
-                        false);
-                }
-                return GetBase64(bmp);
-            }
-        }
-
+        
         #region helper
         private static string GetBase64(GcBitmap bmp)
         {
